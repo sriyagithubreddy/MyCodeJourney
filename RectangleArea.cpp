6b853cf95166df8b666a8a2a8336e86c0e5e8e87
sriyagithubreddy <162871215+sriyagithubreddy@users.noreply.github.com>
2024-09-29 22:10:49 +0530

    Add files via upload

---
+++ RectangleArea.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+class Rectangle{
+    public:
+    int height,width;
+    void display(){
+        cout<<width<<" "<<height<<endl;
+    }
+};
+class RectangleArea: public Rectangle{
+    public:
+    void read_input(){
+        cin>>width>>height;
+    }
+    void display(){
+        cout<<(width*height);
+    }
+};
+
+
+
+int main()
+{
+    RectangleArea r_area;
+    
+    r_area.read_input();
+    
+    r_area.Rectangle::display();
+  
+    r_area.display();
+    
+    return 0;
+}
